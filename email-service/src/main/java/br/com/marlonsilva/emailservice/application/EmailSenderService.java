309f6a49ad2d193c264897b72997d32cc50e82a1
MarlonDB-003 <marlondeividmd@gmail.com>
2024-03-19 09:56:24 -0400

    update EmailSenderService

--- email-service/src/main/java/br/com/marlonsilva/emailservice/application/EmailSenderService.java
+++ email-service/src/main/java/br/com/marlonsilva/emailservice/application/EmailSenderService.java
@@ -17,7 +17,7 @@ public class EmailSenderService implements EmailSenderUseCase{
     }
 
     @Override
-    public void emailSend(String to, String subject, String body) {
+    public void sendEmail(String to, String subject, String body) {
         this.emailSenderGateway.sendEmail(to, subject, body);
     }
     
