0a27280eb35b96052cfee3be137b9b30dd37ae93
MarlonDB-003 <marlondeividmd@gmail.com>
2024-03-19 09:40:20 -0400

    criação classe EmailServiceException

---
+++ email-service/src/main/java/br/com/marlonsilva/emailservice/core/exceptions/EmailServiceException.java
@@ -0,0 +1,12 @@
+package br.com.marlonsilva.emailservice.core.exceptions;
+
+public class EmailServiceException extends RuntimeException {
+   
+    public EmailServiceException(String message){
+        super(message);
+    }
+
+    public EmailServiceException(String message, Throwable cause){
+        super(message, cause);
+    }
+}
