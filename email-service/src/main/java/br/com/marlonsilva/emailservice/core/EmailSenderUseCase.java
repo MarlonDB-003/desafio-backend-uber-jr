57bf2c4a0509407c86ededa1dcf07bbd801bc383
MarlonDB-003 <marlondeividmd@gmail.com>
2024-03-19 09:56:36 -0400

    update EmailSenderUseCase

--- email-service/src/main/java/br/com/marlonsilva/emailservice/core/EmailSenderUseCase.java
+++ email-service/src/main/java/br/com/marlonsilva/emailservice/core/EmailSenderUseCase.java
@@ -2,6 +2,6 @@ package br.com.marlonsilva.emailservice.core;
 
 public interface EmailSenderUseCase {
 
-    void emailSend(String to, String subject, String body);
+    void sendEmail(String to, String subject, String body);
     
 }
