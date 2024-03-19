b46a8aa3860adcc0b1ebfc449f57344a71e2cb51
MarlonDB-003 <marlondeividmd@gmail.com>
2024-03-19 10:03:05 -0400

    criação classe AwsSesConfig

---
+++ email-service/src/main/java/br/com/marlonsilva/emailservice/infra/ses/AwsSesConfig.java
@@ -0,0 +1,16 @@
+package br.com.marlonsilva.emailservice.infra.ses;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+
+@Configuration
+public class AwsSesConfig {
+    
+    @Bean
+    public AmazonSimpleEmailService amazonSimpleEmailService(){
+        return AmazonSimpleEmailServiceClientBuilder.standard().build();
+    }
+}
